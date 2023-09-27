7d84158151bfee781de8eaeb1c2f2562802d0448
Slloth <fiquet.noah@gmail.com>
2023-09-27 12:31:12 +0200

    Création de la partie Vue

--- src/Middleware/AbstractController.php
+++ src/Middleware/AbstractController.php
@@ -4,8 +4,16 @@
 #[AllowDynamicProperties]
 abstract class AbstractController{
     public function loadModel(string $model){
-        //récupère le nom d'un modèle et le charge dans le controller enfant
+        //Récupère le nom d'un modèle et le charge dans le controller enfant
         require_once(ROOT.'src/Model/'.$model.'.php');
         $this->$model = new $model();
     }
+
+    public function render(string $ficher, array $options = []){
+        // Récupère les valeurs du tableau pour en faire des variables à part entière
+        extract($options);
+        // Récupère le fichier php de la vue, a partire du nom du controller pour selectionner le bon dossier
+        require_once(ROOT.'templates/'.strtolower(str_replace('Controller','',get_class($this))).'/'. $ficher.'.php');
+    }
+
 }
