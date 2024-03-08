d70ddf96c369ddf811f89710867afa848a7d8804
Slloth <fiquet.noah@gmail.com>
2024-03-08 14:41:17 +0100

    :zap: load css

--- App/Views/layouts/base.php
+++ App/Views/layouts/base.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon">
-    <link rel="stylesheet" href="assets/css/asset.css">
+    <link rel="stylesheet" href="assets/css/assets.css">
     <script src="assets/js/app.js"></script>
     <title><?=isset($title)?$title:"Document";?></title>
 </head>
